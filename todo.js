c5da10e53196918ada9c7629dca1c2fb949d75cd
kisung1037 <kisung1037@gmail.com>
2019-09-27 21:32:35 +0900

    first

---
+++ todo.js
@@ -0,0 +1,47 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+
+const toDos = [];
+
+
+function paintToDo(text){
+    const li = document.createElement("li");
+    const delBtn = document.createElement('button')
+    delBtn.innerText = "X";
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.id = newId;
+    toDoList.appendChild(li);
+    const toDoObj = {
+         text : text,
+         id: newId,
+    }
+    toDos.push(toDoObj);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDos(){
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+    }
+
+}
+
+function init() {
+    loadToDos();
+    toDoForm.addEventListener("submit" , handleSubmit)
+}
+
+init();
